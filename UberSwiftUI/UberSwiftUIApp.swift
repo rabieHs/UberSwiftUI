ddd537a7fa54f898da9aa0714f05cdb9e8067b1a
rabieHs <rabiehoussaini@gmail.com>
2024-07-17 14:57:50 +0100

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by rabie houssaini on 17/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
